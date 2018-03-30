7fcbba618367c171214fcc1c9fdcd4057fc47903
ru.nekit.android <ru.nekit.android@gmail.com>
2018-03-30 15:11:56 +1000

    add function to time provider interface

--- questDomain/src/main/java/ru/nekit/android/qls/domain/providers/ITimeProvider.kt
+++ questDomain/src/main/java/ru/nekit/android/qls/domain/providers/ITimeProvider.kt
@@ -1,11 +1,12 @@
 package ru.nekit.android.qls.domain.providers
 
-import ru.nekit.android.qls.domain.model.StatisticPeriodType
+import ru.nekit.android.qls.domain.model.StatisticsPeriodType
 
 interface ITimeProvider {
 
     fun getCurrentTime(): Long
 
-    fun getTimestampBy(statisticPeriodType: StatisticPeriodType): Long
+    fun getTimestampBy(statisticsPeriodType: StatisticsPeriodType): Long
 
+    fun getPeriodIntervalForPeriod(statisticsPeriodTypePair: Pair<StatisticsPeriodType, StatisticsPeriodType>): List<Pair<Long, Long>>
 }
