df55b03cf38db908d402600df64f978784691920
ru.nekit.android <ru.nekit.android@gmail.com>
2018-04-02 16:23:39 +1000

    View Holder - add rootContainer

--- sharedModule/src/main/java/ru/nekit/android/utils/ViewHolder.kt
+++ sharedModule/src/main/java/ru/nekit/android/utils/ViewHolder.kt
@@ -4,9 +4,10 @@ import android.content.Context
 import android.support.annotation.LayoutRes
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 
-open class ViewHolder(protected val context: Context, @LayoutRes layoutId: Int) {
+open class ViewHolder(protected val context: Context, @LayoutRes layoutId: Int, root: ViewGroup? = null) {
 
-    val view: View = LayoutInflater.from(context).inflate(layoutId, null)
+    val view: View = LayoutInflater.from(context).inflate(layoutId, root)
 
 }
