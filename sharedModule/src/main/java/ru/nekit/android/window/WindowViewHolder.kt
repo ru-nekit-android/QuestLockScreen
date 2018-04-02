907dc4f01cbe5d7fcf0f6d44c50b971743bc19b1
ru.nekit.android <ru.nekit.android@gmail.com>
2018-04-02 16:26:23 +1000

    Window View Holder - add rootContainer

--- sharedModule/src/main/java/ru/nekit/android/window/WindowViewHolder.kt
+++ sharedModule/src/main/java/ru/nekit/android/window/WindowViewHolder.kt
@@ -5,7 +5,8 @@ import android.view.ViewGroup
 import ru.nekit.android.shared.R
 import ru.nekit.android.utils.ViewHolder
 
-internal class WindowViewHolder(context: Context) : ViewHolder(context, R.layout.layout_window) {
+internal class WindowViewHolder(context: Context, rootGroup: ViewGroup? = null) :
+        ViewHolder(context, R.layout.layout_window, rootGroup) {
 
     var rootContainer: ViewGroup = view.findViewById(R.id.container_root)
     var contentContainer: ViewGroup = view.findViewById(R.id.container_content)
